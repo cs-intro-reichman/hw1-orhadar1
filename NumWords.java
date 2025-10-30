a9442e42de7c12cf262b57f872b85fc475aea3c0
orhadar1 <or.hadar01@post.runi.ac.il>
2025-10-30 13:49:51 +0200

    Finished NumWords

--- NumWords.java
+++ NumWords.java
@@ -2,5 +2,15 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+		int NumWords = Integer.parseInt(args[0]);
+		
+		int ones = NumWords % 10;         
+		int tens = (NumWords / 10) % 10;  
+		int hundreds = (NumWords / 100) % 10; 
+
+		System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
+
+		// % java NumWords 8
+		// 0 hundreds, 0 tens, and 8 ones.
 	}
 }
